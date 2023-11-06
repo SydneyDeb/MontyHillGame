1c616d7535278200273605fcb7fed2a5e3b27260
Sydney Deb <sydneyd30@nycstudents.net>
2023-11-06 09:16:54 -0500

    added a class

--- src/MHG.java
+++ src/MHG.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MHG {
+public class MHG {
+    private int carDoor;
+    private int GoatDoor1;
+
+    public MHG() {
+        carDoor = (int) (Math.random() * 3) + 1;
+    }
+
+    public int GoatDoor(int userPick) {
+        GoatDoor1 = (int) (Math.random() * 3) + 1;
+        if (GoatDoor1 != carDoor && GoatDoor1 != userPick) {
+            return GoatDoor1;
+        }
+        else {
+            GoatDoor1 = (int) (Math.random() * 3) + 1;
+        }
+        return GoatDoor1;
+    }
+
+    public void winOrLoose(int userPick) {
+        if (userPick == carDoor) {
+            System.out.println("You won the car!");
+        }
+        else {
+            System.out.println("The car was behind door " + carDoor + ". You lost the car but hey, you still won a goat!");
+        }
+    }
 }
--- src/MHGRunner.java
+++ src/MHGRunner.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class MHGRunner {
+import java.util.Scanner;
+class MHGRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MHG round = new MHG();
+        System.out.println("Welcome to the Monty Hall Game!");
+        System.out.println("There are two goats and a car randomly placed behind doors 1, 2, and 3");
+        System.out.println("Where do you think the car is? Enter 1, 2 or 3: ");
+        int userPick = scan.nextInt();
+
+        int revealedDoor = round.GoatDoor(userPick);
+        System.out.println("One of the goats is behind door " + revealedDoor);
+        scan.nextLine();
+        System.out.println("Would you like to switch your guess? Enter Yes or No: ");
+        String userSwitch = scan.nextLine();
+
+        if (userSwitch.equals("Yes") || userSwitch.equals("yes")) {
+            int switchedGuess = 6 - (userPick) - revealedDoor;
+            userPick = switchedGuess;
+            System.out.println("You switched to door " + switchedGuess);
+        }
+        else if (userSwitch.equals("No") || userSwitch.equals("no")) {
+            System.out.println("You stayed with door " + userPick);
+        }
+        else {
+            System.out.println("Please input Yes or No.");
+        }
+
+        round.winOrLoose(userPick);
+    }
 }
